7bb277b5393fe11ca3544f43bc59af19da21c84f
fedor.andreev <fedorr47@gmail.com>
2026-04-29 08:30:43 +0200

    CR-42 Gameplay action-request policy tests

--- tests/unit/GameplayTests/TestGameplayActionPolicy.cpp
+++ tests/unit/GameplayTests/TestGameplayActionPolicy.cpp
@@ -112,12 +112,75 @@ TEST(GameplayActionPolicy, PolicyTableSelectionIsDeterministic)
 
 TEST(GameplayActionPolicy, LowerPriorityRequestCannotOverrideHigherPriorityPending)
 {
+    GameplayActionComponent action{};
+
+    EXPECT_TRUE(QueueGameplayActionRequest(action, GameplayActionRequest{
+        .kind = GameplayActionKind::Jump,
+        .source = GameplayActionRequestSource::Input,
+        .priority = 200
+    }));
+
+    EXPECT_TRUE(QueueGameplayActionRequest(action, GameplayActionRequest{
+        .kind = GameplayActionKind::LightAttack,
+        .source = GameplayActionRequestSource::Input,
+        .priority = 10
+    }));
+
+    EXPECT_EQ(GetGameplayRequestedActionKind(action), GameplayActionKind::Jump);
+    EXPECT_EQ(GetGameplayBufferedActionKind(action), GameplayActionKind::LightAttack);
 }
 
 TEST(GameplayActionPolicy, CommitConsumesPendingAndIsIdempotentWhenNoRequestRemains)
 {
+    GameplayActionComponent action{};
+    action.pending = GameplayActionRequest{
+        .kind = GameplayActionKind::Interact,
+        .source = GameplayActionRequestSource::Interaction,
+        .priority = 50
+    };
+
+    CommitGameplayActionState(action);
+    EXPECT_EQ(action.current, GameplayActionKind::Interact);
+    EXPECT_TRUE(action.busy);
+    EXPECT_FALSE(HasGameplayPendingActionRequest(action));
+
+    CommitGameplayActionState(action);
+    EXPECT_EQ(action.current, GameplayActionKind::Interact);
+    EXPECT_TRUE(action.busy);
+    EXPECT_FALSE(HasGameplayPendingActionRequest(action));
 }
 
 TEST(GameplayActionPolicy, FinishPromotesBufferedThenConsumeAndResetAreSafe)
 {
+    GameplayActionComponent action{};
+    action.current = GameplayActionKind::Jump;
+    action.busy = true;
+    action.buffered = GameplayActionRequest{
+        .kind = GameplayActionKind::LightAttack,
+        .source = GameplayActionRequestSource::Combat,
+        .priority = 10
+    };
+
+    FinishGameplayActionState(action);
+    EXPECT_FALSE(action.busy);
+    EXPECT_EQ(action.current, GameplayActionKind::None);
+    EXPECT_EQ(GetGameplayRequestedActionKind(action), GameplayActionKind::LightAttack);
+    EXPECT_EQ(GetGameplayBufferedActionKind(action), GameplayActionKind::None);
+
+    CommitGameplayActionState(action);
+    EXPECT_EQ(action.current, GameplayActionKind::LightAttack);
+    EXPECT_FALSE(HasGameplayPendingActionRequest(action));
+
+    ResetGameplayActionState(action);
+    EXPECT_EQ(action.current, GameplayActionKind::None);
+    EXPECT_FALSE(action.busy);
+    EXPECT_FALSE(HasGameplayPendingActionRequest(action));
+    EXPECT_FALSE(HasGameplayBufferedActionRequest(action));
+
+    EXPECT_TRUE(QueueGameplayActionRequest(action, GameplayActionRequest{
+        .kind = GameplayActionKind::Interact,
+        .source = GameplayActionRequestSource::Interaction,
+        .priority = 50
+    }));
+    EXPECT_EQ(GetGameplayRequestedActionKind(action), GameplayActionKind::Interact);
 }
